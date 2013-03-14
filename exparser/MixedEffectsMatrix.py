57d220dd61c43f5fc8270554ca037616118463b9
Sebastiaan Mathot <s.mathot@cogsci.nl>
2013-03-14 14:16:01 +0100

    Update MixedEffectsMatrix

--- exparser/MixedEffectsMatrix.py
+++ exparser/MixedEffectsMatrix.py
@@ -28,7 +28,7 @@ class MixedEffectsMatrix(BaseMatrix):
 	"""A matrix containing the results of an Anova analysis"""
 
 	def __init__(self, dm, dv, fixedEffects, randomEffects, continuous=[], \
-		nSim=10000, formula=None):
+		nSim=10000, formula=None, fixedOp='*'):
 
 		"""
 		Constructor. Note that for now all effects are assumed to be factors,
@@ -44,11 +44,17 @@ class MixedEffectsMatrix(BaseMatrix):
 
 		Keyword arguments:
 		continuous		--	a list of effects that should be treated as
-							continuous, rather than discrete (default=[])
-		nSim				--	the number of simulations to estimate the P values
+							continuous, rather than discrete. Instead of a list,
+							you can also pass the string 'all' the treat all
+							effects as continuous (default=[])
+		nSim			--	the number of simulations to estimate the P values
 							(default=10000)
 		formula			--	use a specific formular for R, or None to
 							auto-generate (default=None)
+		fixedOp			--	the operator to use for the fixed effects
+							(default='*')
+		pVals			--	indicates whether p-values should be estimated
+							(default=True)
 		"""
 
 		from rpy2 import robjects
@@ -65,10 +71,14 @@ class MixedEffectsMatrix(BaseMatrix):
 		self.dm = dm
 
 		# Construct a formula for R, which should be of the type
-		# 'dv ~ fixed1 * fixed 2  + (1|random1) + (1+random2)'
+		# 'dv ~ fixed1 * fixed 2 + (1|random1) + (1|random2)'
+		# Random slopes example:
+		# 'dv ~ fixed1 * fixed 2 + (1+fixed1|subject) + (1+random2)'
+		
 		if formula == None:
-			fixedEffectsTemplate = '*'.join(fixedEffects)
-			randomEffectsTemplate = '+'.join(['(1|%s)' % re for re in randomEffects])
+			fixedEffectsTemplate = fixedOp.join(fixedEffects)
+			randomEffectsTemplate = '+'.join(['(1|%s)' % re for re in \
+				randomEffects])
 			self._formula = '%s ~ %s + %s' % (dv, fixedEffectsTemplate, \
 				randomEffectsTemplate)
 		else:
@@ -76,23 +86,35 @@ class MixedEffectsMatrix(BaseMatrix):
 
 		# Register all the variables so that R can use them
 		for f in fixedEffects + randomEffects:
-			if f in continuous:
+			if continuous == 'all' or f in continuous:
 				robjects.globalenv[f] = robjects.FloatVector(dm[f])
 			else:
 				robjects.globalenv[f] = robjects.FactorVector(dm[f])
 		robjects.globalenv[sdv] = robjects.FloatVector(dm[dv])
 
 		# Perform the regression
-		model = lme4.lmer(robjects.Formula(self._formula), verbose=False)
-
-		# Estimate p-values
-		self.pVals = langR.pvals_fnc(model, nsim=nSim)
+		self.model = lme4.lmer(robjects.Formula(self._formula), verbose=False)
+		
+		# Get the start of the model output that has t-values and standard
+		# errors
+		lModel = str(self.model).split('\n')
+		for i in range(len(lModel)):
+			if lModel[i] == 'Fixed effects:':
+				break
+		i += 2				
+		self.pVals = langR.pvals_fnc(self.model, nsim=nSim, ndigits=10)
 
 		## Convert the results into a Matrix for easy reading
-		l = [['f0', 'slope', 'Pr(>|t|)', 'sign.']]
+		l = [['f0', 'slope', 'SE', 'ci95low', 'ci95high', 't', 'Pr(>|t|)', \
+			'sign.']]
 		r = 0
 		for row in self.pVals[0].rownames:
-			slope = self.pVals[0][0][r]
+			slope = float(self.pVals[0][0][r])
+			ci95low = float(self.pVals[0][2][r])
+			ci95high = float(self.pVals[0][3][r])
+			print lModel[i+r]
+			t = float(lModel[i+r].split()[3])
+			se = float(lModel[i+r].split()[2])
 			p = float(self.pVals[0][5][r])
 			if p < .001:
 				sign = '***'
@@ -105,7 +127,7 @@ class MixedEffectsMatrix(BaseMatrix):
 			else:
 				sign = ''
 			r += 1
-			l.append([row, slope, p, sign])
+			l.append([row, slope, se, ci95low, ci95high, t, p, sign])
 		self.m = np.array(l, dtype='|S128')
 
 	def formula(self):
