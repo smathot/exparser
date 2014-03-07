7af2d7582b73692dc0f14b6d6c39c1cfd5f6b39e
Sebastiaan Mathot <s.mathot@cogsci.nl>
2014-03-07 11:28:04 +0100

    Add regression plot function

--- exparser/Plot.py
+++ exparser/Plot.py
@@ -19,7 +19,9 @@ along with exparser.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
+import numpy as np
 from matplotlib import pyplot as plt
+from exparser.TangoPalette import *
 
 plotFolder = 'plot'
 if '--clear-plot' in sys.argv and os.path.exists(cacheFolder):
@@ -27,7 +29,12 @@ if '--clear-plot' in sys.argv and os.path.exists(cacheFolder):
 	shutil.rmtree(plotFolder)
 plt.rc('font', family='Arial', size=10)
 
+# Some pre-defined sizes
+xs = 4, 4
+s = 6, 6
 r = 8, 8
+w = 12, 8
+h = 8, 12
 l = 12, 12
 xl = 16, 16
 
@@ -37,13 +44,38 @@ def new(size=r):
 	Creates a new figure.
 
 	Keyword arguments:
-	size	--	The figure size. (default=(12,8))
+	size	--	The figure size. (default=r)
 
 	Returns:
 	A matplotlib figure.
 	"""
 
 	return plt.figure(figsize=size)
+
+def regress(x, y, annotate=True, symbol='.', linestyle='--', symbolColor= \
+	blue[1], lineColor=blue[1]):
+
+	"""
+	Creates a regression plot.
+
+	Arguments:
+	x				--	An array or list for the X data.
+	y			--	An array or list for the Y data.
+
+	Keyword arguments:
+	annotate	--	Indicates whether the correlation and p-value should be
+					marked in the plot. (default=True)
+	"""
+
+	from scipy.stats import linregress
+	s, i, r, p, se = linregress(x, y)
+	plt.plot(x, y, symbol, color=symbolColor)
+	xData = np.array([min(x), max(x)])
+	yData = i + s*xData
+	plt.plot(xData, yData, linestyle, color=lineColor)
+	if annotate:
+		plt.text(0.05, 0.95, 'r = %.3f, p = %.3f' % (r, p), ha='left', \
+			va='top', transform=plt.gca().transAxes)
 
 def save(name, show=False):
 
